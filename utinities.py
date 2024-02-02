f1e5a41a6513dfabfaac681b3b466b3e9fe22471
sonny hsu <unssytaiwan@gmail.com>
2024-02-02 22:52:18 +0800

    config/autofit to excel

--- utinities.py
+++ utinities.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import numpy as np
 from openpyxl import load_workbook
 import json
+from openpyxl.utils import get_column_letter
 import os
 
 def load_config(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         config = json.load(file)
     return config
 
@@ -61,13 +63,42 @@ def save_workbook(file_path, data_range, sheet_name):
     with pd.ExcelWriter(file_path) as writer:
         data_range.to_excel(writer, sheet_name=sheet_name, index=False)
 
+def to_excel_auto_column_width(df: pd.DataFrame, writer: pd.ExcelWriter, sheet_name="Sheet1"):
+    """Write DataFrame to Excel with auto-adjusted column widths"""
+    try:
+        # Write DataFrame to Excel with specified sheet name
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+        # Calculate the character widths for each column header
+        column_widths = df.columns.to_series().apply(lambda x: len(str(x).encode('gbk'))).values
+
+        # Calculate the maximum character width for each column
+        max_widths = df.astype(str).applymap(lambda x: len(str(x).encode('gbk'))).agg(max).values
+
+        # Take the maximum width for each column from the above two calculations
+        widths = np.max([column_widths, max_widths], axis=0)
+
+        # Set the column width for each column in the specified sheet
+        worksheet = writer.sheets[sheet_name]
+        for i, width in enumerate(widths, 1):
+            # Adjusting for openpyxl engine's character width discrepancy
+            worksheet.column_dimensions[get_column_letter(i)].width = width + 2
+
+        # Optionally, you can return some information like the number of rows and columns written
+        return f"Successfully wrote DataFrame to Excel. Rows: {df.shape[0]}, Columns: {df.shape[1]}"
+
+    except PermissionError as e:
+        return f"PermissionError: {e}"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
 def output_data(workbook_path, sheet_name, dataframe):
     try:
         with pd.ExcelWriter(workbook_path, engine='openpyxl', mode='a') as writer:
             if sheet_name in writer.book.sheetnames:
                 writer.book.remove(writer.book[sheet_name])
-
-            dataframe.to_excel(writer, sheet_name=sheet_name, index=False)
+            to_excel_auto_column_width(dataframe, writer, sheet_name=sheet_name)
 
     except PermissionError as e:
         print(f"PermissionError: {e}")
